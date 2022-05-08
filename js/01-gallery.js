f21e23ef1cb7f1128c650d408e64adc170b9a2e1
MarinaHalahan <ichmarina1@gmail.com>
2022-05-08 20:15:08 +0300

    готовое задание

---
+++ js/01-gallery.js
@@ -0,0 +1,57 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryBox = document.querySelector(".gallery");
+
+const createGalleryElement = function () {
+    return  galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${ description } "
+            />
+        </a>
+        </div>
+        `
+    }). join ("");
+};
+
+galleryBox.insertAdjacentHTML("afterbegin", createGalleryElement());
+galleryBox.addEventListener('click', onGalleryItemClick);
+
+let instance;
+
+function onGalleryItemClick(evt) {
+  evt.preventDefault();
+  
+    if(!evt.target.classList.contains('gallery__image')) {
+    return;
+  };
+  const currentItemEl = evt.target;
+  const currentItemUrl = currentItemEl.dataset.source;
+  
+   instance = basicLightbox.create(`<img src="${currentItemUrl}" width="800" height="600">`)
+
+  instance.show();
+  
+  const visible = basicLightbox.visible();
+  if (visible) {
+    window.addEventListener('keydown', onEscKeyPress)
+  };
+};
+
+
+function onEscKeyPress(evt) {
+  if (evt.code === 'Escape') {
+    instance.close();
+    window.removeEventListener('keydown', onEscKeyPress)
+  };
+};
+
+
+
+
+
